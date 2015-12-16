914b4c6c2bf14d6d890d95f21e2743a385d30020
antonmishkovich <anton.mishkovich@gmail.com>
2015-12-16 17:53:40 +0200

    First commit

---
+++ DribbbleAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DribbbleAPI
+//
+//  Created by Anton Mishkovich on 12/16/15.
+//  Copyright © 2015 Anton Mishkovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DribbbleAPI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DribbbleAPI
+//
+//  Created by Anton Mishkovich on 12/16/15.
+//  Copyright © 2015 Anton Mishkovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
